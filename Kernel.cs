26a6cae1b7ecf16053c95e6594ee81cdab1d3dfb
corgifist <86985149+corgifist@users.noreply.github.com>
2022-01-01 22:57:15 +0300

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Sys = Cosmos.System;
+
+namespace Quantum
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            VFS.Init();
+            Console.WriteLine("Quantum boot was success. Entering kernel shell");
+        }
+
+        protected override void Run()
+        {
+            while (true)
+            {
+                string prompt = ShellPrompt();
+                string[] parts = prompt.Split(" ");
+                switch(parts[0])
+                {
+                    case "echo":
+                    {
+                        for (int i = 1; i < parts.Length; i++)
+                        {
+                            Console.Write(parts[i] + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    }
+
+                    case "cdr":
+                    {
+                        break;
+                    }
+                    case "shutdown":
+                    {
+                        Sys.Power.Shutdown();
+                        break;
+                    }
+                }
+            }
+        }
+
+        public string ShellPrompt()
+        {
+            Console.Write("|>");
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ VFS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FS = Cosmos.System.FileSystem;
+
+namespace Quantum
+{
+    class VFS
+    {
+        static FS.CosmosVFS fs = new FS.CosmosVFS();
+        public static void Init()
+        {
+            Cosmos.System.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+        }
+    }
+}
